b86f8a7f4d1a87eaddcb2e7f80307b4dafdbb559
CarlosAlvia <cjalvia@espol.edu.ec>
2024-06-26 14:21:38 -0500

    Requirements 1-4 done

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Hola mundo')
+plans = {"Basic":100,"Premium":200,"Family":300}
+extraFeatures = {"Personal training sessions":25,"Group classes":30}
+
+def show_plans():
+    for index,plan in enumerate(plans.keys()):
+        print(str(index)+")",plan)
+
+def calculate_total_cost(plan,extra_features):
+    plan_cost = plans[plan]
+    extra_cost = 0
+    
+    for extra in extra_features:
+        extra_cost += extraFeatures[extra]
+    
+    return plan_cost + extra_cost
+
+def calculate_group_discount(users,membership_cost):
+    discount = 0
+
+    if users >=2:
+        discount = 0.10 * membership_cost
+
+    return discount
+
+
+
+
