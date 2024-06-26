15244cd182bfd6a534e2c65bfed9a8e1da93d25c
Cliente CONTROL-PC <CltControl@GGIECSOFWRK137.espol.edu.ec>
2024-06-26 14:28:42 -0500

    test calculation

--- test/test.py
+++ test/test.py
@@ -1,32 +1,23 @@
-import pytest
 import main 
 
-def test_total_cost_family():
+def test_calculate_total_cost_family():
     membresia = "Family"
-    features = []
-    response = calculateTotal(membresia, features)
-    assert response == 2
+    features = ["Personal training sessions"]
+    response = main.calculate_total_cost(membresia, features)
+    assert response == 325
 
-def test_total_cost_basic():
+def test_calculate_total_cost_basic():
     membresia = "Basic"
-    features = []
-    response = calculateTotal(membresia, features)
-    assert response == 2
+    features = ["Personal training sessions", "Group classes"]
+    response = main.calculate_total_cost(membresia, features)
+    assert response == 155
 
-def test_total_cost_premium():
+def test_calculate_total_cost_premium():
     membresia = "Premium"
     features = []
-    response = calculateTotal(membresia, features)
-    assert response == 2
+    response = main.calculate_total_cost(membresia, features)
+    assert response == 200
 
 
-def test_special_offer():
-    totalCost = 220
-    response = calculate_discount(totalCost)
-    assert response == 2
 
-def test_especial_offer():
-    totalCost = 600
-    response = calculate_discount(totalCost)
-    assert response == 550
 
